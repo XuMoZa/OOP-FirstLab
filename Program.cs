0469e8d8364c54d6dc08e1ec07fcc3ef16c17f0e
XuMoZa <atapovicmisha@gmail.com>
2022-04-21 12:30:59 +0300

    Commit1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LabWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foreach (Figures ourfigures in Figures.flist) {
+                ourfigures.Print();                
+            }
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabWork1
+{
+    class Square : Figures
+    {
+        public Square(double x1, double x2, double y1, double y2) : base(x1, x2, y1, y2)
+        {
+
+        }
+
+
+        public override void Print()
+        {
+            Console.WriteLine("Square output");
+            base.Print();
+
+        }
+    }
+}
