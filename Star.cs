e1f48ba56b2fbfe4eafa4a0817343d19aa7827ae
XuMoZa <103736238+XuMoZa@users.noreply.github.com>
2022-04-21 12:39:26 +0300

    Add files via upload

---
+++ Star.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabWork1
+{
+    class Star : Figures
+    {
+        public Star (double x1, double x2, double y1, double y2) : base(x1, x2, y1, y2)
+        {
+
+        }
+
+
+        public override void Print()
+        {
+            Console.WriteLine("Star output");
+            base.Print();
+
+        }
+    }
+}
